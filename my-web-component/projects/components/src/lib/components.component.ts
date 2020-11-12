703ecbc9d9d1951101801a8a8cfaa4b5c36e97e6
Mawuli ADJODA <koffimawuli.adjoda@gmail.com>
2020-11-12 08:17:17 +0100

    Create a Component Library for Angular and the Web

---
+++ my-web-component/projects/components/src/lib/components.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-components',
+  template: `
+    <p>
+      components works!
+      author: ADJODA Lefort
+    </p>
+  `,
+  styles: []
+})
+export class ComponentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ my-web-component/projects/elements/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, Injector } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
+import { ComponentsModule, ComponentsComponent } from 'components';
+
+
+@NgModule({
+
+  imports: [
+    BrowserModule,
+    ComponentsModule
+  ],
+  providers: []
+})
+export class AppModule {
+
+  constructor(private injector: Injector){}
+
+  ngDoBootstrap(){
+    const element = createCustomElement(ComponentsComponent, { injector: this.injector })
+    customElements.define("lib-components", element);
+  }
+
+}
